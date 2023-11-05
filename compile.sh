af1f88519ce610de55a664327c9670866b1eca58
shoukaku07 <132239141+Shoukaku07@users.noreply.github.com>
2023-11-05 22:05:03 +0700

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,9 @@
+echo "start compile"
+echo ""
+function compiler() {
+    make -j$(nproc --all) O=out ARCH=arm64 <CODE_NAME>_defconfig
+    make -j$(nproc --all) ARCH=arm64 O=out \
+                          CROSS_COMPILE=aarch64-linux-gnu- \
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+compiler
